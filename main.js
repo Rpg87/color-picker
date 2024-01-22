4007ceb03ef4d9e80fe6d5dffb0388b913b7635d
Rpg87 <sra.picadillo@gmail.com>
2024-01-22 17:14:20 +0100

    add list favorites

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ const copyTxt = document.querySelector('.copyTxt');
 const messageModal = document.getElementById('messageModal');
 const messageText = document.getElementById('messageText');
 
+const heart = document.querySelector('.heart');
+const counter = document.querySelector('.counter');
+const list = document.querySelector('.list');
+
 
 const generateRandomHex = () => {
     let digits = '0123456789ABCDEF'; /* all options for hexadecimal numbers*/
@@ -44,6 +48,37 @@ const showMsg = (msg) => {
 
 };
 
+const addFavorite = () => {
+    let favorite = copyTxt.textContent;
+
+    if (!isFavoriteInList(favorite)) {
+        // Crear un nuevo elemento div con el contenido del favorito
+        const listItem = document.createElement('ul');
+
+        listItem.textContent = favorite;
+
+        // Agregar el nuevo elemento a la lista
+        list.appendChild(listItem);
+
+        // Cambiar el color del corazón
+        heart.style.color = 'red';
+
+        console.log(favorite);
+    }
+
+}
+
+const isFavoriteInList = (favorite) => {
+    const existingItems = list.querySelectorAll('div');
+    for (const item of existingItems) {
+        if (item.textContent === favorite) {
+            return true;
+        }
+    }
+    return false;
+};
+
+
 
 btn.addEventListener('click', changeColor)
 
@@ -51,3 +86,5 @@ copyTxt.addEventListener('click', function () {
     userCopyTxt(copyTxt.innerText);
 
 })
+
+heart.addEventListener('click', addFavorite)
